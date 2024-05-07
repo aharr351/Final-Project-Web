4614ebea18eb84b2b52bdf6fc843830e33d37bfc
autumn <ztsz@iup.edu>
2024-05-07 13:33:32 -0400

    Also  added styling to submittedSolved.php

--- submittedSolved.php
+++ submittedSolved.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Post Closed</title>
+    <link rel="stylesheet" href="submitted.css">
+</head>
+<body>
 <?php
 $postid = $_POST['solved'];
 $conn = new mysqli("localhost", "root", "", "postz");
@@ -7,12 +14,16 @@ $conn = new mysqli("localhost", "root", "", "postz");
 //$querySolved = "SELECT solved FROM posts WHERE postid = $postid";
 //$resultSolved = $db->query($querySolved);
 $sql = "UPDATE posts SET solved='1' WHERE postid=$postid";
-echo "For Post:".$postid."<br>";
+echo "<h1>For Post:".$postid."</h1><br>";
 //Display the solved number
 if ($conn->query($sql)===TRUE) {
-    echo "Your question was closed"."<br>";
-    echo "Thank you for your submission"."<br>";
+    echo "<h1>Your question was closed</h1>"."<br>";
+    echo "<h1>Thank you for your submission</h1>"."<br>";
 }
 ?>
-<p>Return to homepage</p>
+<h1>Return to homepage</h1>
+<div class="align">
 <button onclick="document.location='index.php'">Homepage</button>
+</div>
+</body>
+</html>
